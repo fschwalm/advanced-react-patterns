86e6669f204fa8f825d993e412bfab6ed7c1baa6
Fagner Schwalm <fagnerschwalm@gmail.com>
2022-04-29 19:57:13 -0300

    support DOM component children

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -9,7 +9,9 @@ function Toggle({children}) {
   const toggle = () => setOn(!on)
 
   return React.Children.map(children, child => {
-    return React.cloneElement(child, {on, toggle})
+    return typeof child.type === 'string'
+    ? child
+    : React.cloneElement(child, {on, toggle})
   })
 }
 
@@ -25,6 +27,7 @@ function App() {
       <Toggle>
         <ToggleOn>The button is on</ToggleOn>
         <ToggleOff>The button is off</ToggleOff>
+        <span>test</span>
         <ToggleButton />
       </Toggle>
     </div>
